6572d25fc63df1710b7d908f8358d1392362879b
maricosi <marisaisimoes@gmail.com>
2015-05-05 17:08:43 +0100

    com o cronometro

---
+++ src/main/java/pt/uc/dei/aor/paj/Cronometro.java
@@ -0,0 +1,45 @@
+package pt.uc.dei.aor.paj;
+
+
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+
+@SessionScoped
+public class Cronometro implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	private static long startValue;  
+	private static long stopValue;  
+	private static long timeDiff;  
+
+
+	/** 
+	 * Inicia a contagem temporal 
+	 */  
+	public static void start() {  
+		startValue = System.currentTimeMillis();  
+		stopValue = 0;  
+		timeDiff = 0;  
+	}  
+
+	/** 
+	 * Calcula a diferença temporal 
+	 */  
+	public static void stop() {  
+		stopValue = System.currentTimeMillis();  
+		timeDiff = stopValue - startValue;  
+	}  
+
+	/** 
+	 * Retorna o diferença de tempo medida 
+	 * @return tempo em milisegundos 
+	 */  
+	public static long elapsedTime() {  
+		return timeDiff;  
+	}  
+
+
+}
