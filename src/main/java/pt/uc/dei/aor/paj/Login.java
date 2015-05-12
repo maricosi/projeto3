180294ae35dca8416e154aa361dcd3070e3663a3
ritajrcosta <rita.c@portugalmail.pt>
2015-05-12 14:13:09 +0100

    login a funcionar

--- src/main/java/pt/uc/dei/aor/paj/Login.java
+++ src/main/java/pt/uc/dei/aor/paj/Login.java
@@ -1,41 +1,145 @@
 package pt.uc.dei.aor.paj;
 
-
 import java.io.Serializable;
-import javax.inject.Named;
+import java.util.ArrayList;
+
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 
-@Named("user") // or @ManagedBean(name="user")
+
+@Named
 @SessionScoped
 public class Login implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private String name = "";
-	private String password;
+	private static final long serialVersionUID = -2921010109456538382L;
 
-	public String getName() { 
-		return name; 
+//	@Inject Mensagem chat;
+	@Inject Verificacaologin verifica;
+
+	
+
+
+	//conjunto de utilizadores inscritos
+	private ArrayList<Verificacaologin> utilizadores;
+	//2 utilizadores inscritos por defeito
+
+	//private int numTentativas;
+	private String user, password, mensagem;
+	//variável auxiliar que vai ficar true quando o utilizador fica logado
+	private boolean isLogged=false;
+
+	//construtor cria a ArrayList dos utilizadores e acrescenta os dois por defeito
+	public Login() {
+		super();
+		this.utilizadores = new ArrayList<Verificacaologin>();
+		Verificacaologin ut1=new Verificacaologin();
+		Verificacaologin ut2=new Verificacaologin();
+		ut1.setUsername("Rita");
+		ut1.setPassword("123");
+		ut2.setUsername("Marisa");
+		ut2.setPassword("456");
+		this.utilizadores.add(ut1);
+		this.utilizadores.add(ut2);
+		//this.numTentativas=0;
 	}
 
-	public void setName(String newValue) { 
-		name = newValue;
+	public void setUser(String user) {
+		this.user = user;
 	}
 
-	public String getPassword() { 
-		return password; 
-	}
-
-	public void setPassword(String newValue) { 
-
-		password = newValue; 
-	}
-
-	public String getGreeting() {
-
-		if (name.length() == 0) return "Insira o nome!!";
-		else return "Benvindo, " + name + "!";
+	public void setPassword(String password) {
+		this.password = password;
 
 	}
+
+	//verifica se o utilizador com aquele username já existe ou nao
+	public boolean verifyUser(String username){
+		boolean existe=false;
+		for(Verificacaologin p:utilizadores){
+			if(p.getUsername().equals(username))
+				existe=true;
+		}
+		return existe;
+	}
+
+	//verifica se o utilizador com aquele username já existe ou nao
+	public boolean verifyPass(String pass, String username){
+		boolean existe=false;
+		for(Verificacaologin p:utilizadores){
+			if(p.getUsername().equals(username) && p.getPassword().equals(pass))
+				existe=true;
+		}
+		return existe;
+	}
+
+	//mensagem de loginSucess ou RegistoSucess
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	//logar para utilizador existente
+	//estava void
+	public String logar(){
+
+		//if (this.numTentativas<=3){
+
+			if(verifyPass(this.password, this.user)){
+				this.mensagem = "Bem Vindo ao Sistema "+this.user+"!";
+			//	chat.setUtilizador(this.user);
+				this.user="";
+				this.password="";
+				isLogged=true;
+				verifica.setLogged(isLogged);
+//				this.numTentativas=0;
+				return "basic.xhtml";
+				
+			}else{
+			//	this.numTentativas++;
+				this.mensagem = "Utilizador ou senha Inválidos!";
+				this.user="";
+				this.password="";
+				return "login";
+			}
+	}
+
+	//Acrescenta novo utilizador caso não exista
+	public String novoUtilizador(){
+		boolean existe=false;
+
+		existe=verifyUser(this.user);
+		if (!existe){
+			Verificacaologin ut=new Verificacaologin();
+			ut.setUsername(this.user);
+			ut.setPassword(this.password);
+			this.utilizadores.add(ut);
+		//	chat.setUtilizador(this.user);
+			this.mensagem="Utilizador registado com sucesso!";
+			this.user="";
+			this.password="";
+			return "basic";
+			//this.logged=true;
+		}else{
+			this.mensagem="Utilizador já existente, escolha novo username";
+			this.user="";
+		//	chat.setUtilizador(this.user);
+			this.password="";
+			return "login";
+		}
+	}
+
+
 }
-
